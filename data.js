234544ad0266583dc48bb6766794d0b3124311cd
SAMEER ALREHAILI <salrehaili@MacBook-Pro-3.local>
2025-11-13 13:43:33 +0300

    secure the login form

--- data.js
+++ data.js
@@ -3,5 +3,6 @@ const hours = {
         {"firstName":"Sameer","lastName":"Alrehaili","officeHours":['Sun 2-3', 'Wed 3-4', 'Tues 4-5'] },
         {"firstName":"Anna","lastName":"Smith","officeHours":['Sun 2-3', 'Wed 3-4', 'Tues 4-5'] },
         {"firstName":"Peter","lastName":"Jones","officeHours":['Sun 2-3', 'Wed 3-4', 'Tues 4-5'] }
-    ]
+    ],
+    "hash":"1c7658a1844b243f29f8f3679aff91e648c5296bdc013a4e8a770385be9675fb"
 };
